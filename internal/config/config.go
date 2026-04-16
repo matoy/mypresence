bcd9e4eae673ac4def552cb5d7eb47c204e85e4a
matoy <thomas.texier@gmail.com>
2026-04-16 22:32:29 +0200

    .

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,7 +3,7 @@ package config
 import "os"
 
 // Version is the application version, updated manually for each release.
-const Version = "0.1.9"
+const Version = "0.1.8"
 
 // Config holds all application configuration loaded from environment variables.
 type Config struct {
