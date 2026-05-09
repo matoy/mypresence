4ccfe79506b33b25831a8d4005212924699561bd
matoy <thomas.texier@gmail.com>
2026-05-09 12:35:38 +0200

    version for release 4.1

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Version is the application version, updated manually for each release.
-const Version = "0.4.0"
+const Version = "0.4.1"
 
 // Config holds all application configuration loaded from environment variables.
 type Config struct {
