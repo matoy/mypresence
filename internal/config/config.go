fcb5d810621b42b3084a3f945a934be5962a26b2
matoy <thomas.texier@gmail.com>
2026-09-24 17:51:58 +0200

    chore: bump version to 20260924-1

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Version is the application version, updated manually for each release (format: YYYYMMDD-X).
-const Version = "20260918-2"
+const Version = "20260924-1"
 
 // Config holds all application configuration loaded from environment variables.
 //
